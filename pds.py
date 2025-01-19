b40ecaf107469a454e53e9ecf1bfc5162a1cd0a6
testermac <testermac@khalwenters-MacBook-Air.local>
2025-01-19 12:48:01 +0700

    linkedlist

--- pds.py
+++ pds.py
@@ -2,18 +2,66 @@ class Node:
         def __init__(self, data):
                 self.data = data
                 self.next = None
+
+        def get_value(self):
+                return self.data
+        
+        def get_next_node(self):
+                return self.next
+        
+        def set_next_node(self, next_node):
+                self.next = next_node
+
 class Linkedlist:
-        def __init__(self):
-                self.head = None
-        def append(self, data):
+        def __init__(self, data=None):
+                self.head = Node(data)
+
+        def get_head_node(self):
+                return self.head
+        
+        def insert_new_head(self, data):
+                new_node = Node(data)
+                new_node.set_next_node(self.head)
+                self.head = new_node   
+        
+        def insert_tail_node(self, data):
                 new_node = Node(data)
                 if self.head is None:
                         self.head = new_node
-                else:
-                        current = self.head
-                        while current.next:
-                                current = current.next
-                        current.next = new_node
-linked_list = Linkedlist()
-linked_list.append(1)
-print(linked_list)
+                current = self.head
+                while current.next:
+                        current = current.next
+                current.next = new_node
+        
+        def preview(self):
+                preview = ""
+                current = self.head
+                while current:
+                        if current.get_value() != None:
+                                preview += str(current.get_value()) + "\n"
+                        current = current.get_next_node()
+                return preview
+        
+#correct output is 2 5 2
+
+ii = Linkedlist(5)
+ii.insert_new_head(2)
+ii.insert_tail_node(2)
+print(ii.preview())
+
+
+
+
+
+
+        
+                
+
+
+
+
+
+
+
+
+
